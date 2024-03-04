3fa64d4d0f60a850cf5215eb1b009f799dc729bf
Odilxon <odilxonfarmonqulov6@gmail.com>
2024-03-04 11:06:31 +0500

    help buyrugini qushdik

--- bot.py
+++ bot.py
@@ -10,11 +10,18 @@ bot = TeleBot('6655924429:AAGv3fPhfrvOeFWowXPzLl5pDFEvJ7Pmgps')
 
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def start(message: Message):
     chat_id = message.chat.id
     full_name = message.from_user.full_name
     bot.send_message(chat_id, f"Assalomu alaykum {full_name}")
+
+
+@bot.message_handler(commands=['help'])
+def start(message: Message):
+    chat_id = message.chat.id
+    full_name = message.from_user.full_name
+    bot.send_message(chat_id, f"Assalomu alaykum {full_name} help")
 
 
 @bot.message_handler(content_types=['text', 'photo', 'video', 'animation', 'sticker'])
@@ -22,6 +29,8 @@ def reaction_to_message(message: Message):
     chat_id = message.chat.id
     bot.copy_message(-4178025764, chat_id, message.message_id)
     bot.send_message(-4178025764, f"Bu habar {message.from_user.full_name} dan")
+
+
 
 
 
